599f9d445e41091c8a2fe6da8e7734f491d499a1
NzizaOscar250 <santosjuniorsoftwareengineer1@gmail.com>
2023-07-21 18:29:54 +0300

    added getRandomColor() function  to generate rundom color

--- js/app.js
+++ js/app.js
@@ -8,3 +8,21 @@ function createGrid(size) {
     container.appendChild(cell);
   }
 }
+
+
+createGrid(gridSize);
+
+function clearGrid() {
+  const cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) => (cell.style.backgroundColor = "#fff"));
+}
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
