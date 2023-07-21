ec1a284c949a5a170078bc7c94212b6d7c1a0d28
NzizaOscar250 <santosjuniorsoftwareengineer1@gmail.com>
2023-07-21 18:29:04 +0300

    added createGrid() function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-htmlZ
+const container = document.getElementById("container");
+const gridSize = 16;
+
+function createGrid(size) {
+  for (let i = 0; i < size * size; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    container.appendChild(cell);
+  }
+}
