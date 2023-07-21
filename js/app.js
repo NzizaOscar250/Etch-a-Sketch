889e76eeb7efa5cacd23bf1eef8940ffcd9f3167
NzizaOscar250 <santosjuniorsoftwareengineer1@gmail.com>
2023-07-21 18:30:49 +0300

    added sketch() function  to return output

--- js/app.js
+++ js/app.js
@@ -26,3 +26,14 @@ function getRandomColor() {
   return color;
 }
 
+function sketch() {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+      cell.addEventListener("mouseover", () => {
+        cell.style.backgroundColor = getRandomColor();
+      });
+    });
+  }
+  
+
+  
